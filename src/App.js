11109c59e2f24fabb91538c408936cbc23462e5a
Humberto Goncalves <humberto.g.moreira@gmail.com>
2021-07-26 17:48:20 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  const handleOnClickModal = async () => {
+  const handleOnClickModalWithFilesArray = async () => {
     const filesArray = [
       "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.todamateria.com.br%2Fpaisagem-natural%2F&psig=AOvVaw2Nq_9hScys2e-q6zkjKxIy&ust=1627418570486000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCICpu-DMgfICFQAAAAAdAAAAABAD",
     ];
@@ -18,16 +18,40 @@ function App() {
           ...shareData,
         })
         .then(() => console.log("Share was successful."))
-        .catch((error) => console.log("Sharing failed", error));
+        .catch((error) => {
+          alert(`Sharing failed, error`);
+          console.log("Sharing failed", error);
+        });
     } else {
-      alert(`Your system doesn't support sharing files.`)
+      alert(`Your system doesn't support sharing files.`);
     }
   };
+  const handleOnClickModal = async () => {
+    if (navigator.canShare) {
+      const shareData = {
+        title: "MDN",
+        text: "Learn web development on MDN!",
+        url: "https://developer.mozilla.org",
+      };
 
+      navigator
+        .share(shareData)
+        .then(() => console.log("Share was successful."))
+        .catch((error) => {
+          alert(`Sharing failed, error`);
+          console.log("Sharing failed", error);
+        });
+    } else {
+      alert(`Your system doesn't support sharing files.`);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={handleOnClickModal}>Teste de compartilhamento</button>
+        <button onClick={handleOnClickModalWithFilesArray}>
+          Teste de compartilhamento2
+        </button>
       </header>
     </div>
   );
