ce860528e638d68fee2c76279a6feac242973a97
Humberto <humberto.g.moreira@gmail.com>
2023-07-12 15:47:11 -0300

    add other files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import "./App.css";
 function App() {
   const handleOnClickModalWithFilesArray = async () => {
     const image1 = await fetch("./image1.jpg");
+    const blob1 = await image1.blob();
+    const file1 = new File([blob1], "image.jpg", { type: "image/jpeg" });
+
+    const filesArray = [file1];
+
+    if (navigator.canShare && navigator.canShare({ files: filesArray })) {
+      const shareData = {
+        title: "MDN",
+        text: "Learn web development on MDN!",
+        url: "https://developer.mozilla.org",
+      };
+
+      navigator
+        .share({
+          files: filesArray,
+          ...shareData,
+        })
+        .then(() => console.log("Share was successful."))
+        .catch((error) => {
+          alert(`Sharing failed, error`);
+          console.log("Sharing failed", error);
+        });
+    } else {
+      alert(`Your system doesn't support sharing files.`);
+    }
+  };
+  const handleOnClickModalWithFilesArray2 = async () => {
+    const image1 = await fetch("./image1.jpg");
     const image2 = await fetch("./image2.jpg");
     const blob1 = await image1.blob();
     const blob2 = await image2.blob();
@@ -54,9 +82,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleOnClickModal}>Teste de compartilhamento</button>
+        <button onClick={handleOnClickModal}>Teste de compartilhamento sem imagem</button>
         <button onClick={handleOnClickModalWithFilesArray}>
-          Teste de compartilhamento2
+          Teste de compartilhamento com 1 imagem
+        </button>
+        <button onClick={handleOnClickModalWithFilesArray2}>
+          Teste de compartilhamento com 2 imagens
         </button>
       </header>
       <img src="./image1.jpg" alt="teste1" />
